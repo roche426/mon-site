339a7d4ae4a10f0552ada67dda41227ecf881327
Aurélien Roche <roche426@gmail.com>
2018-10-05 21:49:53 +0200

    mise en production formulaire de contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -34,30 +34,32 @@ class ContactController extends AbstractController
 
         if ($_POST) {
 
-                if (!empty($_POST['firstName']) && !empty($_POST['lastName']) && !empty($_POST['mail']) && !empty($_POST['message'])
-                && $decode['success'] == true) {
+            if (!empty($_POST['firstName']) && !empty($_POST['lastName']) && !empty($_POST['mail']) && !empty($_POST['message'])
+            && $decode['success'] == true) {
 
-                    $firstName = htmlentities($_POST['firstName']);
-                    $lastName = htmlentities($_POST['lastName']);
-                    $email = htmlentities($_POST['mail']);
-                    $message = htmlentities($_POST['message']);
-                    $numberPhone = htmlentities($_POST['phoneNumber']);
+                $firstName = htmlentities($_POST['firstName']);
+                $lastName = htmlentities($_POST['lastName']);
+                $email = htmlentities($_POST['mail']);
+                $messageContact = htmlentities($_POST['message']);
+                $numberPhone = htmlentities($_POST['phoneNumber']);
 
-                    mail('aurelien.roche1@laposte.net', "$firstName $lastName $numberPhone", $message );
+                mail('aurelien.roche1@laposte.net',
+                    "Mon-site : Message de $firstName $lastName : $email/ $numberPhone",
+                    $messageContact
+                );
 
-                    $message = 'Votre message a bien été envoyé, je vous répondrai dans les plus brefs délais';
-                }
-                else {
-                    $inputValue = [
-                        'firstName' => $_POST['firstName'],
-                        'lastName' => $_POST['lastName'],
-                        'mail' => $_POST['mail'],
-                        'message' => $_POST['message'],
-                        'phoneNumber' => $_POST['phoneNumber'],
-                        'recaptcha' => $decode['success']
-                    ];
-                }
-
+                $message = 'Votre message a bien été envoyé, je vous répondrai dans les plus brefs délais';
+            }
+            else {
+                $inputValue = [
+                    'firstName' => $_POST['firstName'],
+                    'lastName' => $_POST['lastName'],
+                    'mail' => $_POST['mail'],
+                    'message' => $_POST['message'],
+                    'phoneNumber' => $_POST['phoneNumber'],
+                    'recaptcha' => $decode['success']
+                ];
+            }
         }
 
         $page = 'contact';
