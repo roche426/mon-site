c216609bbf1b2f96cf0e6806bf84af4b85cbcc90
Aurélien Roche <roche426@gmail.com>
2018-10-05 20:13:33 +0200

    recaptcha

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -18,29 +18,46 @@ class ContactController extends AbstractController
 
     public function index()
     {
+        // Ma clé privée
+        $secret = "6Lf6sHMUAAAAALV4k6RZcWaogxkIGCoVjqvRLbKf";
+        // Paramètre renvoyé par le recaptcha
+        $response = $_POST['g-recaptcha-response'];
+        // On récupère l'IP de l'utilisateur
+        $remoteip = $_SERVER['REMOTE_ADDR'];
+
+        $api_url = "https://www.google.com/recaptcha/api/siteverify?secret="
+            . $secret
+            . "&response=" . $response
+            . "&remoteip=" . $remoteip ;
+
+        $decode = json_decode(file_get_contents($api_url), true);
+
         if ($_POST) {
 
-            if (!empty($_POST['firstName']) && !empty($_POST['lastName']) && !empty($_POST['mail']) && !empty($_POST['message'])) {
+                if (!empty($_POST['firstName']) && !empty($_POST['lastName']) && !empty($_POST['mail']) && !empty($_POST['message'])
+                && $decode['success'] == true) {
 
-                $firstName = htmlentities($_POST['firstName']);
-                $lastName = htmlentities($_POST['lastName']);
-                $email = htmlentities($_POST['mail']);
-                $message = htmlentities($_POST['message']);
-                $numberPhone = htmlentities($_POST['phoneNumber']);
+                    $firstName = htmlentities($_POST['firstName']);
+                    $lastName = htmlentities($_POST['lastName']);
+                    $email = htmlentities($_POST['mail']);
+                    $message = htmlentities($_POST['message']);
+                    $numberPhone = htmlentities($_POST['phoneNumber']);
 
-                mail('aurelien.roche1@laposte.net', "$firstName $lastName $numberPhone", $message );
+                    mail('aurelien.roche1@laposte.net', "$firstName $lastName $numberPhone", $message );
 
-                $message = 'Votre message a bien été envoyé, je vous répondrai dans les plus brefs délais';
-            }
-            else {
-                $inputValue = [
-                    'firstName' => $_POST['firstName'],
-                    'lastName' => $_POST['lastName'],
-                    'mail' => $_POST['mail'],
-                    'message' => $_POST['message'],
-                    'phoneNumber' => $_POST['phoneNumber'],
+                    $message = 'Votre message a bien été envoyé, je vous répondrai dans les plus brefs délais';
+                }
+                else {
+                    $inputValue = [
+                        'firstName' => $_POST['firstName'],
+                        'lastName' => $_POST['lastName'],
+                        'mail' => $_POST['mail'],
+                        'message' => $_POST['message'],
+                        'phoneNumber' => $_POST['phoneNumber'],
+                        'recaptcha' => $decode['success']
                     ];
-            }
+                }
+
         }
 
         $page = 'contact';
